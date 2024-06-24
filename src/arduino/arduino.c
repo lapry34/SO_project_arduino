980919b9dfcacdd42676d42128aaefa73c6586dc
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2024-06-24 12:19:17 +0200

    messo formule carine che fanno calcoli

--- src/arduino/arduino.c
+++ src/arduino/arduino.c
@@ -14,13 +14,13 @@
 #define BUFFER_SIZE 512 // Buffer size for UART communication
 
 static uint8_t online_mode_counter = 0; // Variable to count seconds in online mode
-static uint8_t sampling_counter = 0; // Variable to count samples for current sensor
 
 static uint8_t str_buffer[BUFFER_SIZE] = {0}; // Buffer to store string data
 static uint16_t buffer_len = 0; // Flag to indicate buffer is ready to be sent
 static int8_t online_mode_value = -1;
 
-static uint16_t adc_buffer[SAMPL_FREQ] = {0}; // Buffer to store ADC values
+static uint16_t V_max = 0; // Maximum value of current sensor
+static uint16_t V_min = 1024; // Minimum value of current sensor
 
 // Time struct
 Time time = {0}; //initialize all to 0
@@ -31,13 +31,9 @@ Data data = {0}; //initialize all to 0
 // Timer1 interrupt, every 1 second
 ISR(TIMER1_COMPA_vect) {
 
-    //evaluate the mean of the samples
-    uint16_t adc_value = 0;
-    for(uint8_t i = 0; i < SAMPL_FREQ; i++){
-        adc_value += (adc_buffer[i] / SAMPL_FREQ); //we divide by the number of samples to avoid overflow
-    }
-
-    process_time(&data, &time, adc_value);
+    //evaluate the Vrms from Vpp TODO: add V_bias 
+    uint16_t V_rms = (V_max - V_min) / (2 * 1.414);
+    process_time(&data, &time, V_rms);
 
     if (online_mode_value != -1) {
       online_mode_counter++;
@@ -46,23 +42,26 @@ ISR(TIMER1_COMPA_vect) {
 
         //print the current value to the UART (online mode)
         
-        memcpy(str_buffer, &adc_value, sizeof(uint16_t));
+        memcpy(str_buffer, &V_rms, sizeof(uint16_t));
         str_buffer[sizeof(uint16_t)] = '\n';
         buffer_len = sizeof(uint16_t) + 1;
         UART_putBytes(str_buffer, buffer_len);
       }
     }
+
+    // Reset V_max and V_min
+    V_max = 0; 
+    V_min = 1024; 
 }
 
 // Timer2 interrupt, every 1ms (1kHz)
-ISR(TIMER2_COMPA_vect) { //rovare picchi di corrente massimi e minimi per calcolare cose.
-    // Sample current sensor
-    adc_buffer[sampling_counter] = ADC_read(ADC_PIN);
-    sampling_counter++;
+ISR(TIMER2_COMPA_vect) {
+  // Sample current sensor
+  uint16_t adc_value = ADC_read(ADC_PIN);
 
-    if (sampling_counter >= SAMPL_FREQ) {
-        sampling_counter = 0;
-    }
+  adc_value > V_max ? V_max = adc_value : V_max; //we update the max value
+  adc_value < V_min ? V_min = adc_value : V_min; //we update the min value
+
 }
 
 // UART receive interrupt
@@ -84,9 +83,7 @@ ISR(USART_RX_vect) {
         UART_putBytes(str_buffer, buffer_len);
     }
     else if (received_byte == 'C') {
-        sampling_counter = 0; // Reset sampling counter
         online_mode_counter = 0; // Reset online mode counter
-        memset(&adc_buffer, 0, sizeof(adc_buffer)); // Clear ADC buffer
         memset(&data, 0, sizeof(data)); // Clear data struct
         memset(&time, 0, sizeof(time)); // Clear time struct
     }
@@ -117,6 +114,9 @@ int main(void){
     online_mode_value = atoi((char*)str_buffer+2);
     if(online_mode_value <= 0) online_mode_value = -1; // Invalid input
   }
+
+  //TODO compute v_bias
+
   //end init
   enable_interrupts();
 
