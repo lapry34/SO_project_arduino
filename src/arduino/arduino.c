0a6d13926a42f1d268fb5aa9101467c68471fb38
Gabriele Onorato <onorato.1979543@studenti.uniroma1.it>
2024-06-26 16:50:11 +0200

    add PICCIONE e UART bufferata pazza

--- src/arduino/arduino.c
+++ src/arduino/arduino.c
@@ -15,25 +15,25 @@
 
 #define ROOT_TWO 1.41421356237
 
-static uint8_t online_mode_counter = 0; // Variable to count seconds in online mode
+volatile uint8_t online_mode_counter = 0; // Variable to count seconds in online mode
 
-static uint8_t str_buffer[BUFFER_SIZE] = {0}; // Buffer to store string data
-static uint16_t buffer_len = 0; // Flag to indicate buffer is ready to be sent
-static int8_t online_mode_value = -1;
+volatile uint8_t str_buffer[BUFFER_SIZE] = {0}; // Buffer to store string data
+volatile uint16_t buffer_len = 0; // Flag to indicate buffer is ready to be sent
+volatile int8_t online_mode_value = -1;
 
-static uint16_t V_max = 0; // Maximum value of current sensor
-static uint16_t V_min = 1024; // Minimum value of current sensor
+volatile uint16_t V_max = 0; // Maximum value of current sensor
+volatile uint16_t V_min = 1024; // Minimum value of current sensor
 
 // Time struct
-Time time = {0}; //initialize all to 0
+volatile Time time = {0}; //initialize all to 0
 
 //Data struct
-Data data = {0}; //initialize all to 0
+volatile Data data = {0}; //initialize all to 0
 
 // Timer1 interrupt, every 1 second
 ISR(TIMER1_COMPA_vect) {
 
-    //evaluate the Vrms from Vpp TODO: add V_bias 
+    //evaluate the Vrms from Vpp
     uint16_t V_rms = ((V_max-V_min) >> 2) * ROOT_TWO; //(V_max - V_min) / (2 * 1.414);
     process_time(&data, &time, V_rms);
 
@@ -42,12 +42,10 @@ ISR(TIMER1_COMPA_vect) {
       if (online_mode_counter >= online_mode_value) {
         online_mode_counter = 0;
 
-        //print the current value to the UART (online mode)
-        
+        //put V_rms to buffer to be sent in UART
         memcpy(str_buffer, &V_rms, sizeof(uint16_t));
         str_buffer[sizeof(uint16_t)] = '\n';
-        buffer_len = sizeof(uint16_t) + 1;
-        UART_putBytes(str_buffer, buffer_len);
+        buffer_len = sizeof(uint16_t) + sizeof(char);
       }
     }
 
@@ -68,7 +66,6 @@ ISR(TIMER2_COMPA_vect) {
 
 // UART receive interrupt
 ISR(USART_RX_vect) {
-    disable_interrupts();
 
     unsigned char received_byte = UDR0;
 
@@ -76,13 +73,10 @@ ISR(USART_RX_vect) {
 
     // Respond data request
     if (received_byte == 'Q') {
-        // Send data struct
-
-        //TODO: mettere un flag che si alza quando c'è da stampare, così da consumare meno tempo in ISR
+        // put data struct to buffer to be sent in UART
         memcpy(str_buffer, &data, sizeof(Data));
         str_buffer[sizeof(Data)] = '\n'; 
-        buffer_len = sizeof(Data) + 1;
-        UART_putBytes(str_buffer, buffer_len);
+        buffer_len = sizeof(Data) + sizeof(char);
     }
     else if (received_byte == 'C') {
         online_mode_counter = 0; // Reset online mode counter
@@ -90,8 +84,6 @@ ISR(USART_RX_vect) {
         memset(&time, 0, sizeof(time)); // Clear time struct
     }
 
-
-    enable_interrupts();
 }
 
 
@@ -117,16 +109,17 @@ int main(void){
     if(online_mode_value <= 0) online_mode_value = -1; // Invalid input
   }
 
-  //TODO compute v_bias
-
   //end init
   enable_interrupts();
 
   while(1) {
+    //we wake up and send the buffer
+    if(buffer_len > 0) {
+      UART_putBytes(str_buffer, buffer_len);
+      memset(str_buffer, 0, BUFFER_SIZE);
+      buffer_len = 0;
+    }
     // we go to sleep
-
-    //TODO: spostare qui la stampa dei putBytes con un flag che si alza quando c'è da stampare
-
     sleep_cpu();
   }
 
